82c64e159b7af1a546030ce2d031174f8d02e06b
y-temp4 <y.temp4@gmail.com>
2018-01-12 10:27:16 +0900

    Add SCSS and prettier settings

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  useTabs: false,
+  printWidth: 100,
+  tabWidth: 2,
+  singleQuote: true,
+  semi: false,
+  arrowParens: 'always',
+  trailingComma: 'none',
+  jsxBracketSameLine: false,
+  parser: 'babylon'
+}
