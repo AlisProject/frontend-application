b73d7e893457df79ac8fc02595416d92ff948346
y-temp4 <y.temp4@gmail.com>
2018-01-16 11:57:33 +0900

    Enable to embed youtube video

--- components/atoms/Editor.vue
+++ components/atoms/Editor.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="area-editor-container">
-    <input class="area-title" type="text" @input="onInputTitle" :value="title">
+    <input
+      class="area-title"
+      type="text"
+      placeholder="タイトル"
+      @input="onInputTitle"
+      :value="title">
     <div class="area-body" ref="editable" @input="onInputBody" :value="body"/>
+    <button class="area-youtube-button" @click="onClickButton">Add YouTube iframe</button>
   </div>
 </template>
 
@@ -10,6 +16,7 @@ import { mapMutations } from 'vuex'
 import * as types from '~/store/mutation-types'
 import 'medium-editor/dist/css/medium-editor.min.css'
 import 'medium-editor/dist/css/themes/default.min.css'
+
 const MediumEditor = process.browser ? require('medium-editor') : null
 
 export default {
@@ -30,9 +37,25 @@ export default {
       /* eslint no-new: 0 */
       new MediumEditor(editorElement, {
         placeholder: {
-          text: this.body === '' ? 'Type your text' : ''
+          text: this.body === '' ? 'あなたの物語を教えてください･･･' : ''
         }
       })
+    },
+    onClickButton() {
+      const url = window.prompt('YouTubeのURLを入力してください')
+      const id = this.getYoutubeId(url)
+      const iframe = `<iframe width=560 height=315 src='//www.youtube.com/embed/${id}' frameborder=0 allowfullscreen></iframe>`
+      this.$refs.editable.innerHTML += iframe
+    },
+    getYoutubeId(url) {
+      const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/
+      const match = url.match(regExp)
+
+      if (match && match[2].length === 11) {
+        return match[2]
+      } else {
+        return 'error'
+      }
     },
     onInputTitle({ target: { value: title } }) {
       this.updateTitle({ title })
@@ -49,12 +72,13 @@ export default {
 .area-editor-container {
   display: grid;
   grid-area: editor;
-  grid-template-rows: 50px 500px;
+  grid-template-rows: 50px 500px 50px;
   grid-template-columns: 1082px;
   /* prettier-ignore */
   grid-template-areas:
-    "title"
-    "body ";
+    "title         "
+    "body          "
+    "youtube-button";
 }
 
 .area-title {
@@ -66,5 +90,9 @@ export default {
   grid-area: body;
   width: 100%;
   overflow: scroll;
+}
+
+.area-youtube-button {
+  grid-area: youtube-button;
 }
 </style>
