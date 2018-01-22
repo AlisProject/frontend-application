a49893886046a3aeb49708b3d0198684d31add46
y-temp4 <y.temp4@gmail.com>
2018-01-22 11:57:36 +0900

    Fix to show StoryCard image according to image aspect ratio

--- components/atoms/StoryCardImage.vue
+++ components/atoms/StoryCardImage.vue
@@ -1,17 +1,30 @@
 <template>
-  <img class="area-story-card-image" src='~assets/images/top-story-card-image.png'>
-  <!-- TODO: data属性からCSSに渡す -->
-  <!-- <div class="image" :data-image-url="story.imageUrl"></div> -->
+  <div class="area-story-card-image">
+    <img class="story-image" src='~assets/images/top-story-card-image.png'>
+    <!-- TODO: data属性からCSSに渡す -->
+    <!-- <div class="image" :data-image-url="story.imageUrl"></div> -->
+  </div>
 </template>
 
 <style lang="scss" scoped>
 .area-story-card-image {
   grid-area: story-card-image;
-  width: 100%;
-  max-width: 100%;
+  overflow: hidden;
+
+  .story-image {
+    width: 100%;
+    max-width: 100%;
+  }
 }
 
 .long-story-card .area-story-card-image {
   grid-area: long-story-card-image;
+
+  .story-image {
+    height: 270px;
+    object-fit: cover;
+    object-position: 0%;
+    width: 540px;
+  }
 }
 </style>
