3228052754b61f99a5f30d4d1ffae352e470242e
y-temp4 <y.temp4@gmail.com>
2018-03-02 09:25:42 +0900

    Fix to remove scroll event listener

--- components/atoms/ArticleSideActions.vue
+++ components/atoms/ArticleSideActions.vue
@@ -21,6 +21,9 @@ export default {
   mounted() {
     window.addEventListener('scroll', this.handleScroll)
   },
+  beforeDestroy() {
+    window.removeEventListener('scroll', this.handleScroll)
+  },
   methods: {
     handleScroll() {
       this.scrollY = window.scrollY
