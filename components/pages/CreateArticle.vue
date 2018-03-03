4d8cb188ed74e46aefce881df6251b0426a66ff3
y-temp4 <y.temp4@gmail.com>
2018-03-03 14:36:34 +0900

    Use debounce

--- components/pages/CreateArticle.vue
+++ components/pages/CreateArticle.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="create-article-container">
+    <span>{{ this.saveStatus }}</span>
     <app-header showEditHeaderNav showPostArticleLink class="drafts logo-original"/>
     <editor :tags="this.$store.state.article.tags"/>
     <app-footer/>
@@ -7,6 +8,7 @@
 </template>
 
 <script>
+import debounce from 'lodash.debounce'
 import { mapActions, mapGetters } from 'vuex'
 import AppHeader from '../organisms/AppHeader'
 import Editor from '../atoms/Editor'
@@ -23,37 +25,55 @@ export default {
       articleId: 'article/articleId',
       title: 'article/title',
       body: 'article/body'
-    })
+    }),
+    saveStatus() {
+      if (this.isSaved) {
+        return 'Saved'
+      } else if (this.isSaving) {
+        return 'Saving...'
+      } else {
+        return ''
+      }
+    }
   },
   data() {
     return {
-      isPosted: false
+      isPosted: false,
+      isSaving: false,
+      isSaved: false
     }
   },
   methods: {
     ...mapActions('article', ['postNewArticle', 'putDraftArticle']),
-    async postOrPutArticle() {
+    postOrPutArticle: debounce(async function() {
       const article = {
         title: this.title,
         body: this.body
       }
+      this.isSaving = true
       if (this.isPosted) {
         await this.putDraftArticle({ article, articleId: this.articleId })
+        this.isSaved = true
       } else {
         try {
           await this.postNewArticle({ article })
+          this.isSaved = true
           this.isPosted = true
         } catch (e) {
           console.error(e)
         }
       }
-    }
+    }, 500)
   },
   watch: {
     title(newTitle, oldTitle) {
+      this.isSaved = false
+      this.isSaving = false
       this.postOrPutArticle()
     },
     body(newBody, oldBody) {
+      this.isSaved = false
+      this.isSaving = false
       this.postOrPutArticle()
     }
   }
