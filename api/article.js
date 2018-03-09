a73d793a78f005a1d43081da174043ee6746fd19
HANATANI Takuma <mail@potato4d.me>
2018-03-09 14:24:24 +0900

    Remove unuse functions

--- api/article.js
+++ api/article.js
@@ -1,71 +1,7 @@
 import axios from '~/plugins/axios'
 
-function sendGet(url) {
-  return axios.get(url).then((response) => response.data)
-}
-
-function sendPost(url, data) {
-  return axios.post(url, data).then((response) => response.data)
-}
-
 function sendPut(url, data) {
   return axios.put(url, data).then((response) => response.data)
-}
-
-export function getPopularArticles() {
-  return sendGet('/articles/popular')
-}
-
-export function getNewArticles() {
-  return sendGet('/articles/new')
-}
-
-export function getArticle({ articleId }) {
-  return sendGet(`/articles/${articleId}`)
-}
-
-export function postArticle({ article }) {
-  return sendPost('/me/articles/drafts', article)
-}
-
-export function getLikesCount({ articleId }) {
-  return sendGet(`/articles/${articleId}/like`)
-}
-
-export function getPublicArticlesByUserId({ userId }) {
-  return sendGet('/me/articles/public', { params: { userId } })
-}
-
-export function getDraftArticlesByUserId({ userId }) {
-  return sendGet('/me/articles/drafts', { params: { userId } })
-}
-
-export function getAlisToken({ articleId }) {
-  return axios.get(`/articles/${articleId}/alistoken`).then((response) => response.data)
-}
-
-export function getDraftArticle({ articleId }) {
-  return sendGet(`/me/articles/drafts/${articleId}`)
-}
-
-export function putDraftArticle({ article, articleId }) {
-  return sendPut(`/me/articles/drafts/${articleId}`, article)
-}
-
-export function publishDraftArticle({ article, articleId }) {
-  return sendPut(`/me/articles/drafts/${articleId}/publish`, article)
-}
-
-export function getPublicArticle({ articleId }) {
-  return sendGet(`/me/articles/public/${articleId}`)
-}
-
-export function getEditPublicArticle({ articleId }) {
-  return sendGet(`/me/articles/public/${articleId}/edit`)
-}
-
-export function putPublicArticle({ article, articleId }) {
-  return sendPut(`/me/articles/public/${articleId}/edit`, article)
 }
 
 export function publishPublicArticle({ article, articleId }) {
--- api/user.js
+++
@@ -1,5 +0,0 @@
-import axios from '~/plugins/axios'
-
-export function getUserInfo({ userId }) {
-  return axios.get(`/users/${userId}`).then((response) => response.data)
-}
