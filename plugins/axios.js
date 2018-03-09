361d06c11be47d03f38dec89564bd6ca5d5cb89e
HANATANI Takuma <mail@potato4d.me>
2018-03-09 13:57:09 +0900

    Install @nuxtjs/axios

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,9 +1,3 @@
-import axios from 'axios'
+export default function({ $axios }) {
 
-// axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
-// axios.defaults.withCredentials = true
-// axios.defaults.crossDomain = true
-
-export default axios.create({
-  baseURL: process.env.baseUrl
-})
+}
