0b7e89006580e4b4105f387c3652d5855cfc2496
y-temp4 <y.temp4@gmail.com>
2018-03-26 19:03:06 +0900

    Add login page

---
+++ components/organisms/LoginModal.vue
@@ -0,0 +1,33 @@
+<template>
+  <app-modal title="LOGIN" @close="resetStates">
+    <div slot="modal-content">
+      <login-modal-form />
+    </div>
+  </app-modal>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+import AppModal from '../atoms/AppModal'
+import LoginModalForm from '../molecules/LoginModalForm'
+
+export default {
+  components: {
+    AppModal,
+    LoginModalForm
+  },
+  computed: {
+    ...mapGetters('user', ['sentMail'])
+  },
+  methods: {
+    resetStates() {
+      this.$v.loginModal.$reset()
+      this.hideLoginErrors()
+    },
+    ...mapActions('user', ['setSentMail', 'hideLoginErrors'])
+  },
+  validations: {
+    loginModal: {}
+  }
+}
+</script>
---
+++ pages/login.vue
@@ -0,0 +1,30 @@
+<template>
+  <top-page/>
+</template>
+
+<script>
+import TopPage from '~/components/pages/TopPage'
+
+export default {
+  components: {
+    TopPage
+  },
+  async fetch({ store }) {
+    store.dispatch('user/setLoginModal', { showLoginModal: true })
+
+    await store.dispatch('article/getAllArticles')
+    const { articles } = store.state.article
+    await store.dispatch('article/getUserInfos', { articles })
+    await store.dispatch('article/getAlisTokens', { articles })
+    const { userInfos, alisTokens } = store.state.article
+    store.dispatch('article/setUserInfoToArticles', {
+      articles,
+      userInfos
+    })
+    store.dispatch('article/setAlisTokenToArticles', {
+      articles,
+      alisTokens
+    })
+  }
+}
+</script>
