4ffcaddc949a1c678d7eeb8fd504a34775bb917f
y-temp4 <y.temp4@gmail.com>
2018-03-30 16:11:37 +0900

    Add signup page

---
+++ pages/signup.vue
@@ -0,0 +1,35 @@
+<template>
+  <top-page/>
+</template>
+
+<script>
+import TopPage from '~/components/pages/TopPage'
+
+export default {
+  components: {
+    TopPage
+  },
+  async fetch({ store }) {
+    store.dispatch('user/setSignUpModal', { showSignUpModal: true })
+    // store.dispatch('user/setSignUpAuthFlowLoginModal', { isSignUpAuthFlowLoginModal: true })
+
+    await store.dispatch('article/getAllArticles')
+    const { articles } = store.state.article
+    await store.dispatch('article/getUserInfos', { articles })
+    await store.dispatch('article/getAlisTokens', { articles })
+    const { userInfos, alisTokens } = store.state.article
+    store.dispatch('article/setUserInfoToArticles', {
+      articles,
+      userInfos
+    })
+    store.dispatch('article/setAlisTokenToArticles', {
+      articles,
+      alisTokens
+    })
+  },
+  mounted() {
+    window.scrollTo(0, 0)
+    document.querySelector('html,body').style.overflow = 'hidden'
+  }
+}
+</script>
