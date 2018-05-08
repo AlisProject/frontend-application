36d22a34df1625b7cc88213f83e3c824ec834699
y-temp4 <y.temp4@gmail.com>
2018-05-08 14:50:15 +0900

    Fix design

--- app/components/pages/PublicArticle.vue
+++ app/components/pages/PublicArticle.vue
@@ -39,7 +39,6 @@ export default {
     '...              ...              ...             '
     '...              article          ...             ';
   background: white;
-  position: relative;
 }
 
 .area-article {
