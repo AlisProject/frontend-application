3a301ca9aee512fca14e101414cd9ae4b17a3204
HANATANI Takuma <mail@potato4d.me>
2018-06-01 18:04:18 +0900

    Add presentation module basic spec

---
+++ tests/store/modules/presentation.spec.js
@@ -0,0 +1,30 @@
+import Vuex from 'vuex'
+import { createLocalVue } from '@vue/test-utils'
+import presentation from '@/store/modules/presentation'
+import APIMock from '../../mock/api'
+
+describe('store/modules/presentation.js', () => {
+  let store
+
+  beforeEach(() => {
+    const localVue = createLocalVue()
+    localVue.use(Vuex)
+    store = new Vuex.Store({
+      modules: {
+        presentation
+      }
+    })
+    store.$axios = APIMock
+  })
+
+  afterEach(() => {
+    store = null
+  })
+
+  // Action spec sample
+  test('when dispatch \'setArticleListScrollHeight\' with scrollHeight=100, articleListScrollHeight changed to 100', async () => {
+    const scrollHeight = 100
+    store.dispatch('presentation/setArticleListScrollHeight', { scrollHeight })
+    expect(store.getters['presentation/articleListScrollHeight']).toBe(100)
+  })
+})
