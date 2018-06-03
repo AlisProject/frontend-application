aedb99bacf43d8f367230f21ac1c1d980f1f5d94
matsumatsu20 <matsumatsu20@gmail.com>
2018-06-03 18:16:30 +0900

    Use number format as line-height instead of px

--- app/components/atoms/NotificationCardContentDescription.vue
+++ app/components/atoms/NotificationCardContentDescription.vue
@@ -22,7 +22,7 @@ export default {
   color: #7f7f7f;
   display: -webkit-box;
   font-size: 14px;
-  line-height: 20px;
+  line-height: 1.4;
   margin: 0;
   overflow: hidden;
 }
@@ -31,15 +31,6 @@ export default {
   .area-description {
     -webkit-line-clamp: 2;
     font-size: 12px;
-    line-height: 24px;
-  }
-}
-
-@media screen and (max-width: 550px) {
-  .long-notification-card {
-    .area-overview {
-      line-height: 18px;
-    }
   }
 }
 </style>
