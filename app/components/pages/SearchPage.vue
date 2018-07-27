07dd51c9e8ab7efc1c8b947f2113ed9d70110b55
y-temp4 <y.temp4@gmail.com>
2018-07-27 15:39:36 +0900

    Fix not to use v-show

--- app/components/pages/SearchPage.vue
+++ app/components/pages/SearchPage.vue
@@ -25,18 +25,20 @@
         :class="{ 'selected': !showArticles }">ユーザー</nuxt-link>
     </nav>
     <div class="area-search-result">
-      <template v-show="showArticles">
-        <p class="no-result-message" v-if="searchArticles.articles.length === 0">
-          {{ searchArticles.isFetching || isSearchFirstly ? '' : '該当する検索結果が存在しません。'}}
-        </p>
-        <search-article-card-list :articles="searchArticles.articles" v-else/>
-      </template>
-      <template v-show="!showArticles">
-        <p class="no-result-message" v-if="searchUsers.users.length === 0">
-          {{ searchUsers.isFetching || isSearchFirstly ? '' : '該当する検索結果が存在しません。'}}
-        </p>
-        <search-user-card-list :users="searchUsers.users" v-else/>
-      </template>
+      <no-ssr>
+        <div v-if="showArticles">
+          <p class="no-result-message" v-if="searchArticles.articles.length === 0">
+           {{ searchArticles.isFetching || isSearchFirstly ? '' : '該当する検索結果が存在しません。'}}
+          </p>
+          <search-article-card-list :articles="searchArticles.articles" v-else/>
+        </div>
+        <div v-else>
+          <p class="no-result-message" v-if="searchUsers.users.length === 0">
+           {{ searchUsers.isFetching || isSearchFirstly ? '' : '該当する検索結果が存在しません。'}}
+          </p>
+          <search-user-card-list :users="searchUsers.users" v-else/>
+        </div>
+      </no-ssr>
     </div>
     <the-loader :isLastPage="!this.query || searchArticles.isLastPage"/>
     <app-footer/>
