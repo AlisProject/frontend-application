ec5f2b3b7d9c18ef28a44f275159c1e41818b4dc
HANATANI Takuma <mail@potato4d.me>
2018-07-27 14:04:34 +0900

    Fix guideline typo

--- app/pages/token-use-guidline.vue
+++ app/pages/token-use-guidline.vue
@@ -1,28 +1,11 @@
 <template>
-  <token-use-guidline/>
+  <div />
 </template>
 
 <script>
-import TokenUseGuidline from '~/components/pages/TokenUseGuidline'
-
 export default {
-  components: {
-    TokenUseGuidline
-  },
-  head: {
-    title: 'ALISトークン利用ガイドライン',
-    meta: [
-      {
-        hid: 'og:title',
-        property: 'og:title',
-        content: 'ALISトークン利用ガイドライン | ALIS'
-      },
-      {
-        hid: 'og:description',
-        property: 'og:description',
-        content: 'ALISトークン利用ガイドライン'
-      }
-    ]
+  fetch({ redirect }) {
+    redirect(301, '/token-use-guideline')
   }
 }
 </script>
