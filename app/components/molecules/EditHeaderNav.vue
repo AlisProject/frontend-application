2dfd1e45ca0deeb1280294e2d9fc9032c71d34f8
y-temp4 <y.temp4@gmail.com>
2018-08-06 18:43:24 +0900

    Fix position of edit header nav links

--- app/components/molecules/EditHeaderNav.vue
+++ app/components/molecules/EditHeaderNav.vue
@@ -41,7 +41,10 @@ export default {
   display: grid;
   text-align: center;
   grid-template-rows: 1fr 38px 1fr;
-  grid-template-columns: minmax(64px, 1fr) 56px 42px 108px 1fr 160px 1fr;
+  // 320px - half width of .area-editor-container
+  // 190px - width      of .area-logo
+  // 40px  - margin     of this grid-column-gap
+  grid-template-columns: calc(50vw - 320px - 190px - 40px) 56px 42px 108px 1fr 160px 1fr;
   grid-column-gap: 40px;
   /* prettier-ignore */
   grid-template-areas:
