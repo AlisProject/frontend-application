61492d05cb0a1fc49dd773f3aed9f43e2b01795f
y-temp4 <y.temp4@gmail.com>
2018-08-11 15:32:08 +0900

    Fix not to use SSR

--- app/pages/articles/popular.vue
+++ app/pages/articles/popular.vue
@@ -9,26 +9,26 @@ export default {
   components: {
     PopularArticleList
   },
-  async fetch({ store, query }) {
-    const topic = query.topic || 'crypto'
-    await store.dispatch('article/getTopics')
-    store.dispatch('article/setTopicDisplayName', { topicName: topic })
-    store.dispatch('article/resetArticleData')
-    await store.dispatch('article/getPopularArticles', { topic })
+  async mounted() {
+    const topic = this.$route.query.topic
+    await this.$store.dispatch('article/getTopics')
+    this.$store.dispatch('article/setTopicDisplayName', { topicName: topic })
+    this.$store.dispatch('article/resetArticleData')
+    await this.$store.dispatch('article/getPopularArticles', { topic })
   },
   head() {
     return {
-      title: `${this.$store.state.article.topicDisplayName} - 人気記事`,
+      title: `人気記事`,
       meta: [
         {
           hid: `og:title`,
           property: 'og:title',
-          content: `${this.$store.state.article.topicDisplayName} - 人気記事 | ALIS`
+          content: `人気記事 | ALIS`
         },
         {
           hid: `og:description`,
           property: 'og:description',
-          content: `${this.$store.state.article.topicDisplayName}人気記事一覧`
+          content: `人気記事一覧`
         }
       ]
     }
--- app/pages/articles/recent.vue
+++ app/pages/articles/recent.vue
@@ -9,26 +9,26 @@ export default {
   components: {
     NewArticleList
   },
-  async fetch({ store, query }) {
-    const topic = query.topic || 'crypto'
-    await store.dispatch('article/getTopics')
-    store.dispatch('article/setTopicDisplayName', { topicName: topic })
-    store.dispatch('article/resetArticleData')
-    await store.dispatch('article/getNewPagesArticles', { topic })
+  async mounted() {
+    const topic = this.$route.query.topic
+    await this.$store.dispatch('article/getTopics')
+    this.$store.dispatch('article/setTopicDisplayName', { topicName: topic })
+    this.$store.dispatch('article/resetArticleData')
+    await this.$store.dispatch('article/getNewPagesArticles', { topic })
   },
   head() {
     return {
-      title: `${this.$store.state.article.topicDisplayName} - 新着記事`,
+      title: `新着記事`,
       meta: [
         {
           hid: `og:title`,
           property: 'og:title',
-          content: `${this.$store.state.article.topicDisplayName} - 新着記事 | ALIS`
+          content: `新着記事 | ALIS`
         },
         {
           hid: `og:description`,
           property: 'og:description',
-          content: `${this.$store.state.article.topicDisplayName}新着記事一覧`
+          content: `新着記事一覧`
         }
       ]
     }
