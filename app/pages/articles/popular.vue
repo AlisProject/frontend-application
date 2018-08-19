799726ae5c47d96e4cd2510ce4bd340fbd97b921
y-temp4 <y.temp4@gmail.com>
2018-08-19 20:33:36 +0900

    Fix to catch topic error

--- app/pages/articles/popular.vue
+++ app/pages/articles/popular.vue
@@ -9,19 +9,23 @@ export default {
   components: {
     PopularArticleList
   },
-  async fetch({ store, query, from = {} }) {
-    // 人気記事の初期化
-    // 人気・新着記事の切り替えを行った場合のみ記事データの初期化を行う。
-    // 記事から遷移してきた場合は、スクロール位置を保持させたいので初期化はしない。
-    if (from.name === 'articles-recent') {
-      store.dispatch('article/resetArticleData')
-    }
+  async fetch({ store, query, from = {}, error }) {
+    try {
+      // 人気記事の初期化
+      // 人気・新着記事の切り替えを行った場合のみ記事データの初期化を行う。
+      // 記事から遷移してきた場合は、スクロール位置を保持させたいので初期化はしない。
+      if (from.name === 'articles-recent') {
+        store.dispatch('article/resetArticleData')
+      }
 
-    store.dispatch('article/setArticleType', { articleType: 'popularArticles' })
-    const { topic } = query
-    await store.dispatch('article/getTopics')
-    store.dispatch('article/setTopicDisplayName', { topicName: topic })
-    await store.dispatch('article/getPopularArticles', { topic })
+      store.dispatch('article/setArticleType', { articleType: 'popularArticles' })
+      const { topic } = query
+      await store.dispatch('article/getTopics')
+      store.dispatch('article/setTopicDisplayName', { topicName: topic })
+      await store.dispatch('article/getPopularArticles', { topic })
+    } catch (e) {
+      error({ statusCode: 404 })
+    }
   },
   head() {
     return {
