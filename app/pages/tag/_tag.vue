f03e190aa90fd73ac7f79d98e24e99cf697d7af6
y-temp4 <y.temp4@gmail.com>
2018-08-29 13:43:38 +0900

    Fix to show correct articles

--- app/pages/tag/_tag.vue
+++ app/pages/tag/_tag.vue
@@ -10,14 +10,19 @@ export default {
     TagArticleList
   },
   async fetch({ store, params, error, from = {} }) {
+    const { tag = null } = params
+
     // タグに関連する記事の初期化
     // 記事もしくはユーザーページから遷移してきた場合は、スクロール位置を保持させたいので初期化はしない。
-    if (from.name !== 'user-articles-articleId' && from.name !== 'users-userId') {
+    // 現在表示中のタグに関連する記事のタグと、新しく取得するタグに関連する記事のタグが異なる場合は初期化を行う。
+    if (
+      (from.name !== 'user-articles-articleId' && from.name !== 'users-userId') ||
+      store.state.article.tagArticles.currentTag !== tag
+    ) {
       store.dispatch('article/resetTagArticlesData')
       store.dispatch('presentation/setTagArticlesScrollHeight', { scrollHeight: 0 })
     }
 
-    const { tag = null } = params
     if (tag === null || (tag && tag.length > 25)) {
       error({ statusCode: 404 })
       return
