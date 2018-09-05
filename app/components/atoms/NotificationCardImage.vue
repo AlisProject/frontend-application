16d4c39c150f5de71570418815068805bc74f0aa
y-temp4 <y.temp4@gmail.com>
2018-09-05 20:11:37 +0900

    Revert "[WIP] Add notification design for tip"
    This reverts commit 5e57175a1c9293952ef9f2030e4e960fad94b739.

--- app/components/atoms/NotificationCardImage.vue
+++ app/components/atoms/NotificationCardImage.vue
@@ -4,7 +4,7 @@
       class="profile-icon"
       src="~assets/images/pc/common/icon_like.png"
       v-if="notification.type === 'like'">
-    <template v-if="notification.type === 'comment' || notification.type === 'tip'">
+    <template v-if="notification.type === 'comment'">
       <img
         class="profile-icon"
         :src="notification.userInfo.icon_image_url"
@@ -14,9 +14,6 @@
         src="~assets/images/pc/common/icon_user_noimg.png"
         v-else>
     </template>
-    <div
-      class="tip-error-icon"
-      v-if="notification.type === 'tip_error'"/>
   </div>
 </template>
 
@@ -46,17 +43,6 @@ export default {
     height: 64px;
     width: 64px;
   }
-
-  .tip-error-icon {
-    background: url('~assets/images/pc/notification/icon_exclamation.png') no-repeat;
-    background-position: 27px;
-    background-size: 10px;
-    cursor: pointer;
-    border-radius: 50%;
-    border: 2px solid #f06273;
-    height: 64px;
-    width: 64px;
-  }
 }
 
 @media screen and (max-width: 640px) {
@@ -64,16 +50,6 @@ export default {
     padding: 0px;
 
     .profile-icon {
-      height: 40px;
-      width: 40px;
-    }
-
-    .tip-error-icon {
-      background-position: 17px;
-      background-size: 6px;
-      cursor: pointer;
-      border-radius: 50%;
-      border: 2px solid #f06273;
       height: 40px;
       width: 40px;
     }
--- app/store/modules/user.js
+++ app/store/modules/user.js
@@ -458,7 +458,6 @@ const actions = {
         notifications.map(async (notification) => {
           let userInfo
           if (notification.type === 'comment') {
-            // if (notification.type === 'comment' || notification.type === 'tip') {
             userInfo = await this.$axios.$get(`/users/${notification.acted_user_id}/info`)
           }
           return { ...notification, userInfo }
