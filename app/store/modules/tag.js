37338c94149924311a0cf1541a0d7ff6b86eca27
y-temp4 <y.temp4@gmail.com>
2018-09-17 18:13:39 +0900

    Fix not to show no search result message on tag search page

--- app/store/modules/tag.js
+++ app/store/modules/tag.js
@@ -5,7 +5,8 @@ const namespaced = true
 const state = () => ({
   searchTags: {
     tags: [],
-    page: 1
+    page: 1,
+    isFetching: false
   }
 })
 
@@ -15,10 +16,13 @@ const getters = {
 
 const actions = {
   async getSearchTags({ commit, state }, { query }) {
+    if (state.searchTags.isFetching) return
+    commit(types.SET_SEARCH_TAGS_IS_FETCHING, { isFetching: true })
     const limit = 10
     const tags = await this.$axios.$get('/search/tags', {
       params: { limit, query, page: state.searchTags.page }
     })
+    commit(types.SET_SEARCH_TAGS_IS_FETCHING, { isFetching: false })
     commit(types.SET_SEARCH_TAGS, { tags: tags.map((tag) => tag.name) })
   },
   resetSearchTags({ commit }) {
@@ -29,6 +33,9 @@ const actions = {
 const mutations = {
   [types.SET_SEARCH_TAGS](state, { tags }) {
     state.searchTags.tags = tags
+  },
+  [types.SET_SEARCH_TAGS_IS_FETCHING](state, { isFetching }) {
+    state.searchTags.isFetching = isFetching
   }
 }
 
