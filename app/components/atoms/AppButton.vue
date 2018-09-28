be84d2d525dbad18000b07a1bd88438a968aab36
y-temp4 <y.temp4@gmail.com>
2018-09-28 22:22:57 +0900

    Update design of AppButton

--- app/components/atoms/AppButton.vue
+++ app/components/atoms/AppButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="app-button" :class="{ disabled }" @click="callback($event)">
+  <button class="app-button" :disabled="disabled" @click="callback($event)">
     <slot />
   </button>
 </template>
@@ -22,40 +22,77 @@ export default {
 
 <style lang="scss" scoped>
 .app-button {
-  background: linear-gradient(#858dda);
   border-radius: 18px;
   border: none;
   color: #fff;
   cursor: pointer;
   display: block;
   font-size: 14px;
+  height: 36px;
+  line-height: 36px;
   outline: none;
-  padding: 10px;
+  position: relative;
   text-align: center;
   text-decoration: none;
-  width: 265px;
+  width: 256px;
+  z-index: 0;
+
+  &:before {
+    background: #858dda;
+    border-radius: 18px;
+    content: '';
+    height: 100%;
+    left: 0;
+    position: absolute;
+    top: 0;
+    transition: all 400ms ease;
+    width: 100%;
+    z-index: -1;
+  }
+
+  &:after {
+    background: linear-gradient(314.72deg, #232538 0%, #858dda 100%);
+    border-radius: 18px;
+    content: '';
+    height: 100%;
+    left: 0;
+    position: absolute;
+    top: 0;
+    width: 100%;
+    z-index: -2;
+  }
+
+  &:hover:enabled:before,
+  &:focus:before {
+    opacity: 0;
+  }
 
   &:hover,
   &:focus {
-    background: linear-gradient(314.72deg, #232538 0%, #858dda 100%);
     box-shadow: 0 3px 10px 0 rgba(0, 0, 0, 0.25);
-    transition: all 400ms ease;
   }
 
   &:target {
     display: block;
   }
 
-  &.disabled {
-    background: white;
+  &[disabled] {
+    box-shadow: 0 3px 10px 0 rgba(0, 0, 0, 0.1);
     color: #6e6e6e;
     cursor: default;
-    box-shadow: 0 3px 10px 0 rgba(0, 0, 0, 0.1);
+
+    &:before,
+    &:after {
+      background: #fff;
+    }
   }
 
   a {
     color: #fff;
+    display: block;
     text-decoration: none;
+    width: 100%;
   }
 }
 </style>
+
