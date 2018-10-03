1b9d5b52e92c76e1fce87307c4b0614a432b0bcb
HANATANI Takuma <mail@potato4d.me>
2018-10-03 20:27:59 +0900

    format with prettier

--- app/pages/me/articles/new/mobile.vue
+++ app/pages/me/articles/new/mobile.vue
@@ -87,97 +87,102 @@ export default {
 </script>
 
 <style>
-@import "~~/node_modules/alis-editor/dist/AlisEditor.css";
+@import '~~/node_modules/alis-editor/dist/AlisEditor.css';
 #mobile-editor-wrapper {
   max-width: 424px;
   margin: 0 auto;
 }
-      html {
-        font-size: 10px;
-      }
-      body {
-        font-size: 14px;
-      }
-      html, body {
-        margin: 0;
-        padding: 0;
-        width: 100vw;
-        min-height: 100vh;
-        font-family: sans-serif;
-        overflow-x: hidden;
-      }
+html {
+  font-size: 10px;
+}
+body {
+  font-size: 14px;
+}
+html,
+body {
+  margin: 0;
+  padding: 0;
+  width: 100vw;
+  min-height: 100vh;
+  font-family: sans-serif;
+  overflow-x: hidden;
+}
 
-      * {
-        max-width: 100vw;
-      }
+* {
+  max-width: 100vw;
+}
 
-      body textarea,
-      body .paragraph,
-      h2, h3, h4, h5{
-        font-family: "HelveticaNeue-Light","Helvetica Neue Light","Helvetica Neue","Helvetica","Arial","Lucida Grande","sans-serif",Sans-Serif;
-        line-height: 1.5;
-        letter-spacing: 1.5px;
-      }
+body textarea,
+body .paragraph,
+h2,
+h3,
+h4,
+h5 {
+  font-family: 'HelveticaNeue-Light', 'Helvetica Neue Light', 'Helvetica Neue', 'Helvetica', 'Arial',
+    'Lucida Grande', 'sans-serif', Sans-Serif;
+  line-height: 1.5;
+  letter-spacing: 1.5px;
+}
 
-      * {
-        box-sizing: border-box;
-      }
+* {
+  box-sizing: border-box;
+}
 
-      p {
-        margin: 0.25em 0;
-      }
+p {
+  margin: 0.25em 0;
+}
 
-      #app {
-        width: 900px;
-        margin: 0 auto;
-        position: relative;
-      }
+#app {
+  width: 900px;
+  margin: 0 auto;
+  position: relative;
+}
 
-      a,
-      a:visited {
-        color: #4372E0;
-        display: inline-block;
-        margin: 0 4px;
-        font-weight: 400;
-      }
+a,
+a:visited {
+  color: #4372e0;
+  display: inline-block;
+  margin: 0 4px;
+  font-weight: 400;
+}
 
-      #log {
-        margin-top: 40px;
-        color: #fff;
-        height: 200px;
-        overflow: auto;
-        background: #141414;
-      }
-      #log pre {
-        width: 900px;
-        padding: 20px;
-        margin: 0 auto;
-      }
+#log {
+  margin-top: 40px;
+  color: #fff;
+  height: 200px;
+  overflow: auto;
+  background: #141414;
+}
+#log pre {
+  width: 900px;
+  padding: 20px;
+  margin: 0 auto;
+}
 
-      @media (max-width: 768px) {
-        html {
-          font-size: 8px;
-        }
-        .insert-button {
-          display: none !important;
-        }
-        /* .export-button {
+@media (max-width: 768px) {
+  html {
+    font-size: 8px;
+  }
+  .insert-button {
+    display: none !important;
+  }
+  /* .export-button {
           display: none !important;
         } */
-        #log {
-          display: none;
-        }
-      }
+  #log {
+    display: none;
+  }
+}
 
-      @media (min-width: 768px) {
-        .mobile-insert {
-          display: none !important;
-        }
-      }
+@media (min-width: 768px) {
+  .mobile-insert {
+    display: none !important;
+  }
+}
 
-      @media (max-width: 400px) {
-        #app::before {
-          height: 100vw;
-        }
-      }
+@media (max-width: 400px) {
+  #app::before {
+    height: 100vw;
+  }
+}
 </style>
