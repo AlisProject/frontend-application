8357a7b26f011a43521ee27803e202d0d8f2dea8
HANATANI Takuma <mail@potato4d.me>
2018-10-03 20:25:09 +0900

    Install AlisEditor v0.1.1

---
+++ app/pages/me/articles/new/mobile.vue
@@ -0,0 +1,183 @@
+<template>
+  <div id="mobile-editor-wrapper">
+    <alis-editor
+      :initialState="blocks"
+    />
+  </div>
+</template>
+
+<script>
+import AlisEditor from 'alis-editor'
+import uuid from 'uuid/v4'
+
+export default {
+  components: {
+    AlisEditor
+  },
+  data() {
+    return {
+      blocks: [
+        {
+          id: uuid(),
+          type: 'Heading',
+          payload: {
+            size: 'h2'
+          },
+          children: [
+            {
+              id: uuid(),
+              type: 'Text',
+              payload: {
+                body: '第一章 うさぎの穴をまっさかさま'
+              }
+            }
+          ]
+        },
+        {
+          id: uuid(),
+          type: 'Heading',
+          payload: {
+            size: 'h3'
+          },
+          children: [
+            {
+              id: uuid(),
+              type: 'Text',
+              payload: {
+                body: '第一節 白うさぎ'
+              }
+            }
+          ]
+        },
+        {
+          id: uuid(),
+          type: 'Paragraph',
+          payload: {
+            body:
+              '<p>でもそのうさぎがほんとうに、チョッキのポケットから懐中時計かいちゅうどけいをとりだしてそれをながめ、そしてまたあわててかけだしたとき、アリスもとびあがりました。というのも、チョッキのポケットなんかがあるうさぎはこれまで見たことがないし、そこからとりだす時計をもっているうさぎなんかも見たことないぞ、というのに急に気がついたからです。</p>'
+          }
+        },
+        {
+          id: '9cf0f329-e53b-4340-a96b-e58a9ef8b61e',
+          type: 'Image',
+          payload: {
+            src: 'https://i.imgur.com/c4zRkB7.png'
+          },
+          children: []
+        },
+        {
+          id: 'fewaarfa-123rewgrszb-t42qgvzaeg-awetw',
+          type: 'Paragraph',
+          payload: {
+            body:
+              '<p>そこで、きょうみしんしんになったアリスは、うさぎのあとを追っかけて野原をよこぎって、それがしげみの下の、おっきなうさぎの穴にとびこむのを、ぎりぎりのところで見つけました。</p>'
+          }
+        },
+        {
+          id: uuid(),
+          type: 'Paragraph',
+          payload: {
+            body: 'http://www.gutenberg.org/ebooks/11'
+          }
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+@import "~~/node_modules/alis-editor/dist/AlisEditor.css";
+#mobile-editor-wrapper {
+  max-width: 424px;
+  margin: 0 auto;
+}
+      html {
+        font-size: 10px;
+      }
+      body {
+        font-size: 14px;
+      }
+      html, body {
+        margin: 0;
+        padding: 0;
+        width: 100vw;
+        min-height: 100vh;
+        font-family: sans-serif;
+        overflow-x: hidden;
+      }
+
+      * {
+        max-width: 100vw;
+      }
+
+      body textarea,
+      body .paragraph,
+      h2, h3, h4, h5{
+        font-family: "HelveticaNeue-Light","Helvetica Neue Light","Helvetica Neue","Helvetica","Arial","Lucida Grande","sans-serif",Sans-Serif;
+        line-height: 1.5;
+        letter-spacing: 1.5px;
+      }
+
+      * {
+        box-sizing: border-box;
+      }
+
+      p {
+        margin: 0.25em 0;
+      }
+
+      #app {
+        width: 900px;
+        margin: 0 auto;
+        position: relative;
+      }
+
+      a,
+      a:visited {
+        color: #4372E0;
+        display: inline-block;
+        margin: 0 4px;
+        font-weight: 400;
+      }
+
+      #log {
+        margin-top: 40px;
+        color: #fff;
+        height: 200px;
+        overflow: auto;
+        background: #141414;
+      }
+      #log pre {
+        width: 900px;
+        padding: 20px;
+        margin: 0 auto;
+      }
+
+      @media (max-width: 768px) {
+        html {
+          font-size: 8px;
+        }
+        .insert-button {
+          display: none !important;
+        }
+        /* .export-button {
+          display: none !important;
+        } */
+        #log {
+          display: none;
+        }
+      }
+
+      @media (min-width: 768px) {
+        .mobile-insert {
+          display: none !important;
+        }
+      }
+
+      @media (max-width: 400px) {
+        #app::before {
+          height: 100vw;
+        }
+      }
+</style>
