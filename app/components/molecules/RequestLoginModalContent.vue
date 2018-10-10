d19ad1eee4a911d765b631089afc22da6901ddf8
y-temp4 <y.temp4@gmail.com>
2018-10-10 14:44:27 +0900

    Change design of request login modal

--- app/components/molecules/RequestLoginModalContent.vue
+++ app/components/molecules/RequestLoginModalContent.vue
@@ -3,45 +3,19 @@
     <p class="confirm-text">
       {{ confirmText }}
     </p>
-    <div class="left">
-      <p class="for-submitted-user">
-        β版登録済みの方
-      </p>
-      <p class="description">
-        既に登録済みの方は以下のボタンよりログイン
-        画面へお進みください。また新規登録がお進み
-        でない方は新規登録画面へお進みください。
-      </p>
-      <app-button
-        @click="showLoginModal"
-        class="login-button">
-        ログインする
-      </app-button>
-      <app-button
-        @click="showSignUpModal"
-        class="signup-button">
-        新規登録する
-      </app-button>
-    </div>
-    <div class="right">
-      <p class="for-not-submitted-user">
-        β版未登録の方
-      </p>
-      <p class="description">
-        β版の登録がお済みでない方は大変恐縮です
-        が、ウェイティングリストへのご登録をお願い
-        致しております。ご登録の上、利用開始メール
-        送付までお待ち下さい。
-      </p>
-      <app-button class="submit-button">
-        <a
-          href="http://eepurl.com/drM0Jr"
-          target="_blank"
-          rel="noopener noreferrer nofollow">
-          ウェイティングリストに登録する
-        </a>
-      </app-button>
-    </div>
+    <p class="description">
+      以下のボタンよりログイン画面または新規登録画面へお進みください
+    </p>
+    <app-button
+      @click="showLoginModal"
+      class="login-button">
+      ログインする
+    </app-button>
+    <app-button
+      @click="showSignUpModal"
+      class="signup-button">
+      新規登録する
+    </app-button>
   </div>
 </template>
 
@@ -104,38 +78,15 @@ export default {
 
 .confirm-text {
   color: #030303;
-  font-size: 16px;
-  font-weight: 500;
-  letter-spacing: 0.2px;
-  line-height: 21px;
+  font-size: 14px;
+  letter-spacing: 0.8px;
   text-align: center;
   padding: 30px 0 0;
 }
 
-.left {
-  width: 50%;
-  float: left;
-}
-
-.right {
-  width: 50%;
-  float: right;
-}
-
-.for-submitted-user,
-.for-not-submitted-user {
-  color: #030303;
-  font-size: 14px;
-  font-weight: 500;
-  letter-spacing: 0.17px;
-  line-height: 21px;
-  text-align: center;
-  margin: 50px 0 0;
-}
-
 .description {
   @include default-text();
-  width: 255px;
+  max-width: 394px;
   margin: 20px auto 0;
 }
 
@@ -144,47 +95,26 @@ export default {
 }
 
 .signup-button {
-  margin: 30px auto 60px;
-}
-
-.submit-button {
-  margin: 40px auto 0;
-}
-
-@media screen and (max-width: 820px) {
-  .left,
-  .right {
-    width: 100%;
-    float: none;
-  }
+  margin: 20px auto 60px;
 }
 
 @media screen and (max-width: 550px) {
   .confirm-text {
-    padding: 4vh 0 0;
+    padding: calc(50vh - 160px) 0 0;
+  }
+
+  .description {
+    max-width: 300px;
   }
 
   .login-button {
     margin: 40px auto 0;
   }
-
-  .signup-button {
-    margin: 20px auto 40px;
-  }
-
-  .submit-button {
-    margin: 40px auto 0;
-  }
 }
 
 @media screen and (max-width: 320px) {
-  .for-submitted-user {
-    margin: 40px 0 0;
-  }
-
   .login-button,
-  .signup-button,
-  .submit-button {
+  .signup-button {
     width: 220px;
     margin: 12px auto;
   }
