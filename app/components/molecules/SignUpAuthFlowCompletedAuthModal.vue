c6f388ec2ac64004531b3e364cecd719ab8fb79a
y-temp4 <y.temp4@gmail.com>
2018-10-10 16:16:00 +0900

    Update design of SignUpAuthFlowCompletedAuthModal

--- app/components/molecules/SignUpAuthFlowCompletedAuthModal.vue
+++ app/components/molecules/SignUpAuthFlowCompletedAuthModal.vue
@@ -1,26 +1,16 @@
 <template>
-  <div>
-    <div class="modal-body">
-      <p class="announce">
-        ALISへの登録が完了しました！
-      </p>
-      <p class="description">
-        引き続き電話番号の登録が完了すると、自分で作成した記事へいいねをもらったり、誰かの記事にいいねを行うとALISトークンがゲットできます！
-        ぜひたくさん使ってALISトークンをゲットしてください！
-      </p>
-      <img
-        class="completed-auth-img"
-        src="~/assets/images/pc/common/image_certification.jpg"
-        alt="completed-auth-image">
-    </div>
-    <div class="modal-footer">
-      <app-button class="to-next-step-button" @click="transitToNextStep">
-        次へ
-      </app-button>
-      <button class="to-top-button" @click="transitToTop">
-        TOPに戻る
-      </button>
-    </div>
+  <div class="modal-body">
+    <p class="announce">
+      ALISへの<span class="br"/>登録が完了しました！
+    </p>
+    <p class="description">
+      次のステップで電話番号による認証が完了すると、
+      誰かの記事にいいねをしたり、自分の書いた記事がいいねされることで
+      ALISトークンを獲得できるようになります。
+    </p>
+    <app-button class="to-next-step-button" @click="transitToNextStep">
+      次へ
+    </app-button>
   </div>
 </template>
 
@@ -61,59 +51,37 @@ export default {
 <style lang="scss" scoped>
 .modal-body {
   margin: 0 auto;
-
-  .announce {
-    color: #030303;
-    font-size: 20px;
-    font-weight: 500;
-    letter-spacing: 1.14px;
-    line-height: 1.5px;
-    margin: 60px 0 0;
-    text-align: center;
-  }
-
-  .description {
-    @include default-text();
-    margin: 20px auto 0;
-    max-width: 400px;
-  }
-
-  .completed-auth-img {
-    width: 280px;
-    margin: 24px auto 0;
-    display: block;
-  }
 }
 
-.modal-footer {
-  width: 270px;
-  margin: 40px auto 40px;
+.announce {
+  color: #030303;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 1.14px;
+  line-height: 1.5;
+  margin: 60px 0 0;
+  text-align: center;
+}
 
-  .to-next-step-button {
-    margin: 20px auto 0;
-  }
+.description {
+  color: #030303;
+  font-size: 14px;
+  font-weight: 500;
+  letter-spacing: 0.17px;
+  line-height: 2;
+  margin: 40px auto 0;
+  max-width: 400px;
+}
 
-  .to-top-button {
-    background: #fff;
-    border-radius: 18px;
-    border: 1px solid #858dda;
-    color: #858dda;
-    cursor: pointer;
-    display: block;
-    font-size: 14px;
-    margin: 20px auto 0;
-    outline: none;
-    padding: 10px;
-    text-align: center;
-    text-decoration: none;
-    width: 265px;
+.to-next-step-button {
+  margin: 40px auto;
+}
 
-    &:focus {
-      opacity: 0.8;
-    }
-
-    &:target {
-      display: block;
+@media screen and (max-width: 550px) {
+  .br {
+    &:before {
+      content: '\A';
+      white-space: pre;
     }
   }
 }
