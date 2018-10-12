cc036838b500be1c9ba2d6aca96194514ee25272
HANATANI Takuma <mail@potato4d.me>
2018-10-12 13:46:18 +0900

    fix config

--- nuxt.config.development.js
+++ nuxt.config.development.js
@@ -3,13 +3,15 @@ const nuxtConfig = require('./nuxt.config.js')
 nuxtConfig.axios = {
   prefix: '/api',
   proxyHeaders: false,
-  proxy: true
+  proxy: true,
+  baseURL: process.env.PROXY_TARGET_URL,
+  browserBaseURL: ''
 }
 
 nuxtConfig.proxy = {
   '/api': {
-    target: process.env.PROXY_TARGET_URL
-    // pathRewrite: { '^/api': '/' }
+    target: process.env.PROXY_TARGET_URL,
+    pathRewrite: { '^/api': '/' }
   }
 }
 
