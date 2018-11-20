99e575c47841bf633402222cbcdffb46ef2413e4
y-temp4 <y.temp4@gmail.com>
2018-11-20 15:17:48 +0900

    Update design of navigation

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,10 @@ aws s3 cp app/static/OGP_1200×630.png s3://$DIST_S3_BUCKET_NAME/d/nuxt/dist/
 aws s3 cp app/static/icon_user_noimg.png s3://$DIST_S3_BUCKET_NAME/d/nuxt/dist/
 aws s3 cp app/static/touch-icon.png s3://$DIST_S3_BUCKET_NAME/d/nuxt/dist/
 aws s3 cp app/static/header_logo_original.png s3://$DIST_S3_BUCKET_NAME/d/nuxt/dist/
+
+aws s3 cp app/static/topic/topic_business.png s3://$DIST_S3_BUCKET_NAME/d/nuxt/dist/topic/
+aws s3 cp app/static/topic/topic_crypto.png s3://$DIST_S3_BUCKET_NAME/d/nuxt/dist/topic/
+aws s3 cp app/static/topic/topic_gosyuin.png s3://$DIST_S3_BUCKET_NAME/d/nuxt/dist/topic/
+aws s3 cp app/static/topic/topic_gourmet.png s3://$DIST_S3_BUCKET_NAME/d/nuxt/dist/topic/
+aws s3 cp app/static/topic/topic_illustration-comic.png s3://$DIST_S3_BUCKET_NAME/d/nuxt/dist/topic/
 yarn sls:deploy
