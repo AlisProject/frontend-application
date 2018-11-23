c59d9446e399b6548a6581b03591f693b3c8baf5
y-temp4 <y.temp4@gmail.com>
2018-11-23 20:43:23 +0900

    Fix to redirect to user profile page

--- app/pages/users/_userId/drafts.vue
+++ app/pages/users/_userId/drafts.vue
@@ -9,17 +9,19 @@ export default {
   components: {
     DraftArticleList
   },
-  async fetch({ store, params, error, redirect }) {
+  async fetch({ store, params }) {
     await store.dispatch('user/setUserInfo', { userId: params.userId })
+  },
+  async mounted() {
+    const isCurrentUser =
+      this.$store.state.user.loggedIn &&
+      this.$route.params.userId === this.$store.state.user.currentUser.userId
 
     // ログインユーザーが他人の下書きページにアクセスした場合、記事一覧ページにリダイレクトする
-    const isCurrentUser =
-      store.state.user.loggedIn && params.userId === store.state.user.currentUser.userId
     if (!isCurrentUser) {
-      redirect(`/users/${params.userId}`)
+      this.$router.push(`/users/${this.$route.params.userId}`)
+      return
     }
-  },
-  mounted() {
     this.$store.dispatch('article/getDraftArticles')
   },
   head: {
