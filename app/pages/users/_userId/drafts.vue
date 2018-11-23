624103007994bcf66cfb8504375670254700d1a6
y-temp4 <y.temp4@gmail.com>
2018-11-23 14:56:08 +0900

    Fix not to access to other user drafts page

--- app/pages/users/_userId/drafts.vue
+++ app/pages/users/_userId/drafts.vue
@@ -9,8 +9,15 @@ export default {
   components: {
     DraftArticleList
   },
-  async fetch({ store, params, error }) {
+  async fetch({ store, params, error, redirect }) {
     await store.dispatch('user/setUserInfo', { userId: params.userId })
+
+    // ログインユーザーが他人の下書きページにアクセスした場合、記事一覧ページにリダイレクトする
+    const isCurrentUser =
+      store.state.user.loggedIn && params.userId === store.state.user.currentUser.userId
+    if (!isCurrentUser) {
+      redirect(`/users/${params.userId}`)
+    }
   },
   mounted() {
     this.$store.dispatch('article/getDraftArticles')
