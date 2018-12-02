146d03b10ff17cef941108ed53ccbf6c48cfd34e
y-temp4 <y.temp4@gmail.com>
2018-12-02 13:57:07 +0900

    Update footer content

--- app/components/organisms/AppFooter.vue
+++ app/components/organisms/AppFooter.vue
@@ -1,15 +1,15 @@
 <template>
   <footer class="area-app-footer-container">
     <nuxt-link to="/users/ALIS-official" class="link">公式アナウンス</nuxt-link>
+    <a href="https://intercom.help/alismedia" class="link" target="_blank" rel="noopener noreferrer nofollow">
+      よくある質問・問い合わせ
+    </a>
     <nuxt-link to="/terms" class="link">利用規約</nuxt-link>
     <nuxt-link to="/privacy" class="link">プライバシーポリシー</nuxt-link>
     <nuxt-link to="/token-use-guideline" class="link">ALISトークン利用ガイドライン</nuxt-link>
-    <a href="https://intercom.help/alismedia" class="link" target="_blank" rel="noopener noreferrer nofollow">
-      ヘルプ
-    </a>
-    <nuxt-link to="/users/AB2" class="link">技術ブログ</nuxt-link>
-    <a href="https://github.com/AlisProject/api-docs" class="link" target="_blank">API</a>
     <a href="https://alismedia.jp/ja/" class="link" target="_blank">運営会社</a>
+    <nuxt-link to="/users/AB2" class="link">ALIS技術ブログ</nuxt-link>
+    <a href="https://github.com/AlisProject/api-docs" class="link" target="_blank">開発者向けAPI</a>
   </footer>
 </template>
 
--- app/components/organisms/SubFooter.vue
+++ app/components/organisms/SubFooter.vue
@@ -1,29 +1,29 @@
 <template>
   <footer class="area-sub-footer-container">
     <nuxt-link to="/users/ALIS-official" class="link">公式アナウンス</nuxt-link>
+    <a href="https://intercom.help/alismedia" class="link" target="_blank" rel="noopener noreferrer nofollow">
+      よくある質問・問い合わせ
+    </a>
     <nuxt-link to="/terms" class="link">利用規約</nuxt-link>
     <nuxt-link to="/privacy" class="link">プライバシーポリシー</nuxt-link>
     <nuxt-link to="/token-use-guideline" class="link">ALISトークン利用ガイドライン</nuxt-link>
-    <a href="https://intercom.help/alismedia" class="link" target="_blank" rel="noopener noreferrer nofollow">
-      ヘルプ
-    </a>
-    <nuxt-link to="/users/AB2" class="link">技術ブログ</nuxt-link>
-    <a href="https://github.com/AlisProject/api-docs" class="link" target="_blank">API</a>
     <a href="https://alismedia.jp/ja/" class="link" target="_blank">運営会社</a>
+    <nuxt-link to="/users/AB2" class="link">ALIS技術ブログ</nuxt-link>
+    <a href="https://github.com/AlisProject/api-docs" class="link" target="_blank">開発者向けAPI</a>
   </footer>
 </template>
 
 <style lang="scss">
 .area-sub-footer-container {
   grid-area: sub-footer;
-  padding: 0 48px;
+  padding: 0 28px 0 46px;
 
   .link {
     color: #6e6e6e;
     font-size: 12px;
     font-weight: bold;
     text-decoration: none;
-    margin: 0 20px 14px 0;
+    margin: 0 18px 14px 0;
     white-space: nowrap;
     display: inline-block;
   }
