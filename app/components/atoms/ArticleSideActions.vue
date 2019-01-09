43a0fa9a538f9c4cd1873bd204e26c13688fb5ea
y-temp4 <y.temp4@gmail.com>
2019-01-09 16:38:17 +0900

    Fix position of twitter share icon

--- app/components/atoms/ArticleSideActions.vue
+++ app/components/atoms/ArticleSideActions.vue
@@ -163,7 +163,8 @@ export default {
   .area-share-twitter {
     grid-area: share-twitter;
     background: #fff url('~assets/images/pc/article/icon_share_twitter.png') no-repeat;
-    background-position: 9px;
+    background-position-x: 9px;
+    background-position-y: 9px;
     margin-bottom: 20px;
   }
 
