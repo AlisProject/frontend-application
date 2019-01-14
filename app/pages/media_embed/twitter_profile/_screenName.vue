027de2910b277694e48ccee5b8098733c07c8635
narisada014 <nari.reishi@gmail.com>
2019-01-14 17:52:59 +0900

    iframe read form env

--- app/pages/media_embed/twitter_profile/_screenName.vue
+++ app/pages/media_embed/twitter_profile/_screenName.vue
@@ -17,12 +17,11 @@ export default {
     }
   },
   async mounted() {
-    const API_KEY = '518401c27d170fda5a9fbc'
     console.log(this.$route.params.screenName)
     const twitterProfileName = this.$route.params.screenName
     this.profileName = twitterProfileName
     const response = await axios.get(
-      `https://iframe.ly/api/oembed?api_key=${API_KEY}&url=${encodeURIComponent(`twitter.com/${twitterProfileName}`)}&omit_script=1&omit_css=1`
+      `https://iframe.ly/api/oembed?api_key=${process.env.IFRAMELY_API_KEY}&url=${encodeURIComponent(`twitter.com/${twitterProfileName}`)}&omit_script=1&omit_css=1`
     )
     console.log(response)
     this.title = response.data.title
