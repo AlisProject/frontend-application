8d59b265ab67bf24e20fa4d7193a5c311ff7927c
y-temp4 <y.temp4@gmail.com>
2019-02-01 16:32:44 +0900

    Refactor code

--- app/pages/me/articles/new.vue
+++ app/pages/me/articles/new.vue
@@ -23,23 +23,38 @@ export default {
     EditDraftArticleV2
   },
   async created() {
+    // 下書き記事の最新記事を確認し、空記事だった場合はその記事を表示する
+    // 空記事でなかった場合は新しく記事を作成する
+
     const { Items: articles } = await this.$axios.$get('/me/articles/drafts', {
       params: { limit: 1 }
     })
+    // 下書き記事が存在しない場合は、記事を作成する
+    if (articles.length < 0) {
+      await this.createNewArticle()
+      return
+    }
     const latestDraftArticleId = articles[0] && articles[0].article_id
     const latestDraftArticle = await this.$axios.$get(`/me/articles/${latestDraftArticleId}/drafts`)
     const isLatestDraftArticleEmpty =
       latestDraftArticle.title === null && latestDraftArticle.body === null
     const isV2Article = isV2(latestDraftArticle)
-
     if (isLatestDraftArticleEmpty) {
-      await this.$store.dispatch('article/getEditDraftArticle', { articleId: latestDraftArticleId })
-      this.$store.dispatch('article/setGotArticleData', { gotArticleData: true })
-      this.componentName = isV2Article ? 'EditDraftArticleV2' : 'EditDraftArticleV1'
+      await this.useLatestDraftArticle(latestDraftArticleId, isV2Article)
     } else {
+      await this.createNewArticle()
+    }
+  },
+  methods: {
+    async createNewArticle() {
       const article = { title: '', body: '' }
       await this.$store.dispatch('article/postNewArticle', { article })
       this.componentName = 'CreateArticle'
+    },
+    async useLatestDraftArticle(latestDraftArticleId, isV2Article) {
+      await this.$store.dispatch('article/getEditDraftArticle', { articleId: latestDraftArticleId })
+      this.$store.dispatch('article/setGotArticleData', { gotArticleData: true })
+      this.componentName = isV2Article ? 'EditDraftArticleV2' : 'EditDraftArticleV1'
     }
   },
   head: { ...head, title: '新規記事' }
