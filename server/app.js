39d9c6ea24ef6f7f394722dbc3ff04728a291a5a
narisada014 <nari.reishi@gmail.com>
2019-02-06 15:38:22 +0900

    change same origin

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ const setHeaders = (req, res, next) => {
   res.removeHeader('x-powered-by')
   res.header('no-cache', 'Set-Cookie')
   res.header('x-xss-protection', '1; mode=block')
-  res.header('x-frame-options', 'DENY')
+  res.header('x-frame-options', 'SAMEORIGIN')
   res.header('x-content-type-options', 'nosniff')
   res.header('Cache-Control', 'max-age=120')
   next()
