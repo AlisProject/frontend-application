62720b046720fabd607868f460664f4105e39a42
y-temp4 <y.temp4@gmail.com>
2019-03-26 17:29:42 +0900

    Update layout of paypart page

---
+++ app/components/organisms/SelectPaypartHeader.vue
@@ -0,0 +1,167 @@
+<template>
+  <header class="area-mobile-editor-header-container" :class="{ 'is-fixed': isFixed }">
+    <nuxt-link to="/" class="logo-link" @click.native="resetData">
+      <img class="logo" src="~assets/images/pc/common/header_logo_original.png" alt="logo">
+    </nuxt-link>
+    <span class="save-status">{{ saveStatus }}</span>
+    <button class="post-article" @click="showMobileEditorHeaderPostArticleModal">
+      公開する
+    </button>
+    <mobile-editor-header-post-article-modal v-if="mobileEditorHeaderPostArticleModal.isShow" />
+    <toast position="n" />
+  </header>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+import { Toast } from 'vuex-toast'
+import MobileEditorHeaderPostArticleModal from '../organisms/MobileEditorHeaderPostArticleModal'
+import { isIOS, isAndroid } from '~/utils/device'
+import { getThumbnails } from '~/utils/article'
+
+export default {
+  components: {
+    Toast,
+    MobileEditorHeaderPostArticleModal
+  },
+  data() {
+    return {
+      deviceType: 'pc',
+      isFixed: false
+    }
+  },
+  mounted() {
+    window.addEventListener('scroll', this.handleScroll)
+    if (isIOS()) {
+      this.deviceType = 'ios'
+    } else if (isAndroid()) {
+      this.deviceType = 'android'
+    }
+  },
+  beforeDestroy() {
+    window.removeEventListener('scroll', this.handleScroll)
+  },
+  computed: {
+    ...mapGetters('user', ['loggedIn', 'mobileEditorHeaderPostArticleModal']),
+    ...mapGetters('article', ['saveStatus'])
+  },
+  methods: {
+    resetData() {
+      // 同一のページの場合は記事情報をリセットしない
+      if (this.$route.fullPath === '/') return
+      this.resetArticleData()
+      this.setArticleListScrollHeight({ scroll: 0 })
+    },
+    showMobileEditorHeaderPostArticleModal() {
+      this.setThumbnails()
+      this.setMobileEditorHeaderPostArticleModal({ isShow: true })
+    },
+    setThumbnails() {
+      const images = Array.from(document.querySelectorAll('figure img'))
+      const thumbnails = getThumbnails(images)
+      this.updateSuggestedThumbnails({ thumbnails })
+      if (!thumbnails.includes(this.thumbnail)) {
+        this.updateThumbnail({ thumbnail: '' })
+      }
+    },
+    handleScroll() {
+      this.isFixed = window.scrollY >= 1
+    },
+    ...mapActions('presentation', ['setArticleListScrollHeight']),
+    ...mapActions('article', ['resetArticleData', 'updateSuggestedThumbnails', 'updateThumbnail']),
+    ...mapActions('user', ['setMobileEditorHeaderPostArticleModal'])
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.pc,
+.ios,
+.android {
+  .area-mobile-editor-header-container {
+    grid-area: mobile-editor-header;
+    z-index: 2002;
+    display: flex;
+    align-items: center;
+    position: relative;
+    width: 1080px;
+    margin: 0 auto;
+
+    &.is-fixed {
+      box-shadow: 0 12px 12px -12px rgba(192, 192, 192, 0.7);
+      left: 0;
+      position: fixed;
+      right: 0;
+    }
+  }
+
+  .logo-link {
+    margin: 0 auto;
+
+    .logo {
+      width: 102px;
+      height: auto;
+    }
+  }
+}
+
+@media screen and (max-width: 1080px) {
+  .pc .area-mobile-editor-header-container {
+    max-width: calc(100% - 68px);
+  }
+}
+
+.ios .area-mobile-editor-header-container {
+  position: absolute;
+}
+
+.android .area-mobile-editor-header-container {
+  position: fixed;
+}
+
+.ios,
+.android {
+  .area-mobile-editor-header-container {
+    max-width: 100%;
+    background: #fff;
+    top: 0;
+    right: 0;
+    left: 0;
+    height: 56px;
+    width: 100%;
+  }
+
+  .logo-link {
+    left: 12px;
+    padding: 12px 0;
+    position: absolute;
+
+    .logo {
+      width: auto;
+      height: 26px;
+    }
+  }
+
+  .save-status {
+    color: #6e6e6e;
+    font-size: 12px;
+    position: absolute;
+    right: 90px;
+    width: 48px;
+  }
+
+  .post-article {
+    background-color: #0086cc;
+    border-radius: 2px;
+    border: 0;
+    color: #fff;
+    font-size: 12px;
+    height: 24px;
+    outline: none;
+    padding: 0;
+    position: absolute;
+    right: 12px;
+    width: 60px;
+  }
+}
+</style>
