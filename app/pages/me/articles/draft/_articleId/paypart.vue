af362da24012cb6833ffe4994ad7035d7ae6fb54
y-temp4 <y.temp4@gmail.com>
2019-03-29 21:18:11 +0900

    Fix to check paypart value and Redirect to top

--- app/pages/me/articles/draft/_articleId/paypart.vue
+++ app/pages/me/articles/draft/_articleId/paypart.vue
@@ -8,6 +8,10 @@ import SelectPaypart from '~/components/pages/SelectPaypart'
 export default {
   components: {
     SelectPaypart
+  },
+  fetch({ store, redirect }) {
+    const { price, title, body } = store.state.user.selectPayment
+    if (price === '0' || !title || !body) redirect('/')
   }
 }
 </script>
