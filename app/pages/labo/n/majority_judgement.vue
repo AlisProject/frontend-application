fa4089cd8fd216c15e1f6de9cfbfeeb0cdc61fe9
sot528 <sot528@gmail.com>
2019-07-17 03:02:07 +0900

    refactoring

--- app/pages/labo/n/majority_judgement.vue
+++ app/pages/labo/n/majority_judgement.vue
@@ -1,31 +1,13 @@
 <template>
-  <labo-n-majority-judgement :article="article" :topic="topicDisplayName" />
+  <labo-n-majority-judgement />
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
 import LaboNMajorityJudgement from '~/components/pages/LaboNMajorityJudgement'
 
 export default {
   components: {
     LaboNMajorityJudgement
-  },
-  async fetch({ store, query, from = {}, error }) {
-    try {
-      // TODO:
-      // await store.dispatch('article/getLaboNArticleRandom')
-      //
-      // await store.dispatch('article/getTopics')
-      // store.dispatch('article/setTopicDisplayName', {
-      //   topicName: store.state.article.article.topic
-      // })
-    } catch (e) {
-      console.log(e)
-      error({ statusCode: 404 })
-    }
-  },
-  computed: {
-    ...mapGetters('article', ['article', 'topicDisplayName'])
   }
 }
 </script>
