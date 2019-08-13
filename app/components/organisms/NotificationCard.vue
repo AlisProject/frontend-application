0752716ec47faeb334013932b0f1734e6055c031
keillera <keikaillera@gmail.com>
2019-08-13 15:30:56 +0900

    Add notification type of fraud.

--- app/components/organisms/NotificationCard.vue
+++ app/components/organisms/NotificationCard.vue
@@ -18,6 +18,10 @@
       <notification-card-image :notification="notification" />
       <notification-card-content :notification="notification" />
     </a>
+    <div v-else-if="notification.type === 'fraud'" class="notification-card-container">
+      <notification-card-image :notification="notification" />
+      <notification-card-content :notification="notification" />
+    </div>
     <nuxt-link v-else :to="articlePath" class="notification-card-container">
       <notification-card-image :notification="notification" />
       <notification-card-content :notification="notification" />
