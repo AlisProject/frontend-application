af5e54e05b162a7f9666617afea46d9842692a55
keillera <keikaillera@gmail.com>
2022-07-20 21:50:00 +0900

    Add sliver and gold notifications.

--- app/components/atoms/NotificationCardContentDescription.vue
+++ app/components/atoms/NotificationCardContentDescription.vue
@@ -44,14 +44,20 @@
   <p v-else-if="notification.type === 'get_token_like'" class="area-description">
     「いいね」によって
     <span class="gray-darker">{{ formattedAlisToken(notification.token) }}ALIS</span> 獲得しました！
-    <span v-if="isOverNFTToken">
+    <span v-if="isOverGoldFTToken">
       <!--合計-->
       <!--<span class="gray-darker">{{ formattedAlisToken(notification.sum_token) }}ALIS</span>-->
       <!--獲得しています。-->
       NFT獲得条件達成済
     </span>
+    <span v-else-if="isOverSilverNFTToken">
+      GOLD NFT獲得まで <span class="gray-darker">{{ nextNftToken }}ALIS</span> です。
+    </span>
+    <span v-else-if="isOverBronzeNFTToken">
+      SILVER NFT獲得まで <span class="gray-darker">{{ nextNftToken }}ALIS</span> です。
+    </span>
     <span v-else>
-      NFT獲得まで <span class="gray-darker">{{ nftToken }}ALIS</span> です。
+      NFT獲得まで <span class="gray-darker">{{ nextNftToken }}ALIS</span> です。
     </span>
   </p>
   <p v-else-if="notification.type === 'get_token_article'" class="area-description">
@@ -79,13 +85,25 @@ export default {
     articlePriceForUser() {
       return new BigNumber(this.notification.price).div(formatNumber).toString(10)
     },
-    isOverNFTToken() {
+    isOverGoldFTToken() {
+      return BigNumber(this.notification.sum_token).gte(BigNumber(6000 * formatNumber))
+    },
+    isOverSilverNFTToken() {
+      return BigNumber(this.notification.sum_token).gte(BigNumber(3000 * formatNumber))
+    },
+    isOverBronzeNFTToken() {
       return BigNumber(this.notification.sum_token).gte(BigNumber(100 * formatNumber))
     },
-    nftToken() {
-      return this.formattedAlisToken(
-        BigNumber(100 * 10 ** 18).minus(BigNumber(this.notification.sum_token))
-      )
+    nextNftToken() {
+      let tmpToken
+      if (this.isOverSilverNFTToken) {
+        tmpToken = BigNumber(6000 * 10 ** 18).minus(BigNumber(this.notification.sum_token))
+      } else if (this.isOverBronzeNFTToken) {
+        tmpToken = BigNumber(3000 * 10 ** 18).minus(BigNumber(this.notification.sum_token))
+      } else {
+        tmpToken = BigNumber(100 * 10 ** 18).minus(BigNumber(this.notification.sum_token))
+      }
+      return this.formattedAlisToken(tmpToken)
     },
     decodedArticleTitle() {
       return htmlDecode(this.notification.article_title)
